97eb56b5124e5f5a3774c6ade3534c3f3f122bde
Aleksey Lovchikov <svdmusic@gmail.com>
2023-08-19 14:10:26 +0300

    Install expo fonts

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,56 @@
 import { StatusBar } from 'expo-status-bar';
-import { Image, StyleSheet, View } from 'react-native';
+import Entypo from '@expo/vector-icons/Entypo';
+import { StyleSheet, Text, View } from 'react-native';
+import {
+  useFonts,
+  Inter_900Black,
+  Inter_400Regular,
+} from '@expo-google-fonts/inter';
+import * as Font from 'expo-font';
+import * as SplashScreen from 'expo-splash-screen';
+import { useCallback, useEffect, useState } from 'react';
 
-import { Game, Home } from '@screens';
+SplashScreen.preventAutoHideAsync();
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Inter_400Regular,
+    Inter_900Black,
+  });
+
+  const [appIsReady, setAppIsReady] = useState(false);
+
+  useEffect(() => {
+    async function prepare() {
+      try {
+        await Font.loadAsync(Entypo.font);
+        await new Promise((resolve) => setTimeout(resolve, 2000));
+      } catch (e) {
+        console.warn(e);
+      } finally {
+        setAppIsReady(true);
+      }
+    }
+
+    prepare();
+  }, []);
+
+  const onLayoutRootView = useCallback(async () => {
+    if (appIsReady && fontsLoaded) {
+      await SplashScreen.hideAsync();
+    }
+  }, [appIsReady, fontsLoaded]);
+
+  if (!appIsReady || !fontsLoaded) {
+    return null;
+  }
+
   return (
-    <View style={styles.container}>
-      <Image source={require('@assets/favicon.png')} />
-      <Home />
-      <Game />
+    <View style={styles.container} onLayout={onLayoutRootView}>
+      <Text style={{ fontSize: 25, fontFamily: 'Inter_900Black' }}>
+        Tic Tac Toe
+      </Text>
+      <Text style={{ fontFamily: 'Inter_400Regular', fontSize: 40 }}>Game</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -17,7 +59,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#eee',
+    backgroundColor: 'white',
     alignItems: 'center',
     justifyContent: 'center',
   },
